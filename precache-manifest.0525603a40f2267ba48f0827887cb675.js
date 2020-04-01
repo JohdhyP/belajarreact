0cac74840a85969741da76ffa1f13c851068e28e
johdhyp <johdhy.prasojo@gmail.com>
2020-04-01 13:26:14 +0800

    Updates

---
+++ precache-manifest.0525603a40f2267ba48f0827887cb675.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc88f00d0e8cbdc2800c5520adecaf9",
+    "url": "/belajarreact/index.html"
+  },
+  {
+    "revision": "f55bba01588417555d1c",
+    "url": "/belajarreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7bd2a78f8eb249ed01b0",
+    "url": "/belajarreact/static/js/2.fa9d16e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/belajarreact/static/js/2.fa9d16e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55bba01588417555d1c",
+    "url": "/belajarreact/static/js/main.2c57375a.chunk.js"
+  },
+  {
+    "revision": "9866e72b29254be2bb2c",
+    "url": "/belajarreact/static/js/runtime-main.9ed93861.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/belajarreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
